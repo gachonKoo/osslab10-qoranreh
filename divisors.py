b51946ed1d72bb557ec544e3d342285c73099098
qoranreh <154002483+qoranreh@users.noreply.github.com>
2024-11-10 14:11:19 +0900

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -2,8 +2,9 @@ import sys
 
 number=int(sys.argv[1])
 
-for i in range(1,number):
-  if (number %=1) ==0:
+for i in range(1,number+1):
+  if (number %=i) ==0:
     print(i,end=" ")
+  
     
 print()
